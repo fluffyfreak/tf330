4dd1a239b639947b60032dd658dc85c42f749c41
TF <sleary@vavi.co.uk>
2020-04-13 01:38:51 +0100

    Update arb.v

--- rtl/arb.v
+++ rtl/arb.v
@@ -3,20 +3,20 @@
     Copyright (C) 2019, Stephen J. Leary
     All rights reserved.
     
-    This file is part of  TF530 (Terrible Fire 030 Accelerator).
+    This file is part of  TF330 (Terrible Fire 030 Accelerator).
 
-    TF530 is free software: you can redistribute it and/or modify
+    TF330 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
-    TF530 is distributed in the hope that it will be useful,
+    TF330 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with TF530. If not, see <http://www.gnu.org/licenses/>.
+    along with TF330. If not, see <http://www.gnu.org/licenses/>.
 */
 
 module arb(
