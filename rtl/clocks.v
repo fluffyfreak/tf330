7c4b9da20d7bb9978d54dd895105d6f434ef646c
TF <sleary@vavi.co.uk>
2020-04-13 01:40:14 +0100

    Update clocks.v

--- rtl/clocks.v
+++ rtl/clocks.v
@@ -1,4 +1,23 @@
+`timescale 1ns / 1ps
+/*
+    Copyright (C) 2019, Stephen J. Leary
+    All rights reserved.
+    
+    This file is part of  TF330 (Terrible Fire 030 Accelerator).
 
+    TF330 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    TF330 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TF330. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 module clocks(
